1395e40b1ac4c59fb21d20455a423611e59986ce
kishore kumar <56813672+kishore0506@users.noreply.github.com>
2021-03-09 09:42:52 +0530

    angular

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,13 +1,15 @@
 export default class Product {
-    constructor(pid:any,prdname:string,description:string,cost:any){
+    constructor(pid:any,prdname:string,description:string,cost:any, type:string){
         this.prdname = prdname;
         this.pid = pid;
         this.description=description;
         this.cost=cost;
+        this.type=type;
     }
     pid:any=0;
     prdname:string="";
     description:string="";
     cost:any="";
+    type:string="";
 
 }
